5218613ef33f17e758f4c8bb11eac0dc74f481a0
genaro gonzalez <genarrogg@gmail.com>
2023-08-02 17:57:21 -0400

    fix

--- src/consultas/index.js
+++ src/consultas/index.js
@@ -4,14 +4,7 @@ const Consultas = () => {
   const {
     logoHome,
     logo,
-    pista1,
-    pista2,
-    pista3,
-    pista4,
-    pista5,
-    pista6,
-    pista7,
-    pista8,
+
     pit,
     nftBg,
 
@@ -32,46 +25,6 @@ const Consultas = () => {
         }
       }
 
-      pista1: file(relativePath: { eq: "home/mapas/pista1.jpg" }) {
-        sharp: childImageSharp {
-          gatsbyImageData(width: 360, quality: 90)
-        }
-      }
-      pista2: file(relativePath: { eq: "home/mapas/pista2.jpg" }) {
-        sharp: childImageSharp {
-          gatsbyImageData(width: 360, quality: 90)
-        }
-      }
-      pista3: file(relativePath: { eq: "home/mapas/pista3.jpg" }) {
-        sharp: childImageSharp {
-          gatsbyImageData(width: 360, quality: 90)
-        }
-      }
-      pista4: file(relativePath: { eq: "home/mapas/pista4.jpg" }) {
-        sharp: childImageSharp {
-          gatsbyImageData(width: 360, quality: 90)
-        }
-      }
-      pista5: file(relativePath: { eq: "home/mapas/pista5.jpg" }) {
-        sharp: childImageSharp {
-          gatsbyImageData(width: 360, quality: 90)
-        }
-      }
-      pista6: file(relativePath: { eq: "home/mapas/pista6.jpg" }) {
-        sharp: childImageSharp {
-          gatsbyImageData(width: 360, quality: 90)
-        }
-      }
-      pista7: file(relativePath: { eq: "home/mapas/pista7.jpg" }) {
-        sharp: childImageSharp {
-          gatsbyImageData(width: 360, quality: 90)
-        }
-      }
-      pista8: file(relativePath: { eq: "home/mapas/pista8.jpg" }) {
-        sharp: childImageSharp {
-          gatsbyImageData(width: 360, quality: 90)
-        }
-      }
       pit: file(relativePath: { eq: "home/pit.png" }) {
         sharp: childImageSharp {
           gatsbyImageData(width: 300, quality: 90)
@@ -95,23 +48,11 @@ const Consultas = () => {
     }
   `)
 
-  const mapas = [
-    pista1.sharp,
-    pista2.sharp,
-    pista3.sharp,
-    pista4.sharp,
-    pista5.sharp,
-    pista6.sharp,
-    pista7.sharp,
-    pista8.sharp,
-  ]
-
   const data = {
     logoHome: logoHome.sharp,
     logo: logo.sharp,
     logoOriginal: logo.sharp.original.src,
 
-    mapas,
     pit: pit.sharp,
     nftBg: nftBg.sharp,
     vueltarapida,
