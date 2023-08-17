30f4e1548fb3697b3ef3f315a117c68805d07b79
genaro gonzalez <genarrogg@gmail.com>
2023-08-17 09:02:24 -0400

    boton para ir al juego

--- src/components/views/home/Welcome.tsx
+++ src/components/views/home/Welcome.tsx
@@ -3,32 +3,21 @@ import Qwelcome from "../../../consultas/home/Qwelcome"
 import Img from "../../nano/Img"
 import MyForm from "./form/MyForm"
 import AuthContext from "../../../context/autenticacion/authContext"
-
+import A from "../../nano/A"
 
 interface WelcomeProps {}
 
 const Welcome: FunctionComponent<WelcomeProps> = () => {
- 
-
-
   const { autenticado } = useContext(AuthContext)
-
 
   return (
     <>
       <div className={`welcome`} id="welcome">
         <Img type="bg" src={Qwelcome().bg} css="background">
-          <div className="container" >
+          <div className="container">
             <Img src={Qwelcome().logo} />
 
-            {autenticado ? null : <MyForm />}
-
-           {/*  <button
-              
-              className="activeForm"
-            >
-              login
-            </button> */}
+            {autenticado ? <A css="toDemo" to="/demo">demo</A> : <MyForm />}
           </div>
         </Img>
       </div>
