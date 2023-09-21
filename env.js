aacdcbcb6db3760948c87202b92c4763926d998b
genaro gonzalez <genarrogg@gmail.com>
2023-09-21 17:09:17 -0400

    env

--- env.js
+++ env.js
@@ -3,6 +3,6 @@
 /* const BACKEND_SIN_DOMINIO = "https://vr-backend-k0xy.onrender.com/api"; */
 const BACKEND_PAGO = "https://vr-backend2.onrender.com/api"
 
-const BACKEND =  BACKEND_LOCAL
+const BACKEND = BACKEND_LOCAL || BACKEND_PAGO || BACKEND_SIN_DOMINIO || BACKEND_CON_DOMINIO
 
 export { BACKEND }
