bf1889e17fbcfd3cc84b413758648cc5837309a5
genaro gonzalez <genarrogg@gmail.com>
2023-11-07 21:11:50 -0400

    fix

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 /* const BACKEND_LOCAL = "http://localhost:3000/api" */
 /* const BACKEND_CON_DOMINIO = "https://backend.vueltarapida.net/api" */
 /* const BACKEND_SIN_DOMINIO = "https://vr-backend-k0xy.onrender.com/api"; */
-const BACKEND_PAGO = "https://vr-backend-9snm.onrender.com//api"
+const BACKEND_PAGO = "https://vr-backend-9snm.onrender.com/api"
 
 const BACKEND = BACKEND_PAGO 
 
